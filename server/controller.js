3f51976f9a0df6d8da8d87e9ce5b494ea4fbf3cd
Alex Wimmel <alex.wimmel@gmail.com>
2026-01-18 18:41:08 -0500

    Improve auth HTML pages
        - Migrate away from basic text and towards better looking page
        - Dynamically replace attributes to avoid needing to create multiple
          .html files

--- server/controller.js
+++ server/controller.js
@@ -1,17 +1,28 @@
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 const authHelper = require("../backend/authHelper.js");
 
 const spotifyApi = "https://api.spotify.com/v1";
+const authFailed = "Authorization Failed!";
+const pagePath = path.join(__dirname, "./", "resp.html");
+const backgroundPath = path.join(__dirname, "../assets", "background.png");
 
 exports.spotifyLogin = async (req, res) => {
 	try {
 		const recState = decodeURIComponent(req?.query?.state);
 		const state = authHelper.getState();
 		authHelper.setState(null);
+
 		if (state !== recState) {
-			throw new Error(
-				"Received state does not match expectations. Finish authentication in less than 30 seconds."
+			res.send(
+				generateHtmlPage(
+					authFailed,
+					"Received state does not match expectations. Finish authentication in less than 30 seconds."
+				)
 			);
+			setTimeout(() => process.exit(0), 100);
+			return;
 		}
 
 		const userCode = decodeURIComponent(req?.query?.code);
@@ -38,7 +49,14 @@ exports.spotifyLogin = async (req, res) => {
 		const accessToken = resp.data?.access_token;
 		const refreshToken = resp.data?.refresh_token;
 		if (!accessToken || !refreshToken) {
-			throw new Error("Invalid tokens received from Spotify. Restart app and try again.");
+			res.send(
+				generateHtmlPage(
+					authFailed,
+					"Invalid tokens received from Spotify. Restart app and try again."
+				)
+			);
+			setTimeout(() => process.exit(0), 100);
+			return;
 		}
 		authHelper.setTokens(accessToken, refreshToken);
 
@@ -47,12 +65,34 @@ exports.spotifyLogin = async (req, res) => {
 		});
 		const userId = meResp.data.id;
 		if (!userId) {
-			throw new Error("Unable to retrieve user id. Restart app and try again.");
+			res.send(
+				generateHtmlPage(authFailed, "Unable to retrieve User ID. Restart app and try again.")
+			);
+			setTimeout(() => process.exit(0), 100);
+			return;
 		}
 		authHelper.setUserId(userId);
 
-		res.send("Authorization successful. Return to app to begin shuffling.");
+		res.send(
+			generateHtmlPage(
+				"Authorization Successful!",
+				"Close this window and return to the app to begin."
+			)
+		);
 	} catch (error) {
 		res.send(error.message);
 	}
 };
+
+function generateHtmlPage(title, desc) {
+	let respPage = fs.readFileSync(pagePath, "utf8");
+	respPage = respPage.replace("{{TITLE}}", title);
+	respPage = respPage.replace("{{DESC}}", desc);
+
+	const imageBuffer = fs.readFileSync(backgroundPath);
+	const base64Image = imageBuffer.toString("base64");
+	const mimeType = "image/png";
+	respPage = respPage.replace("{{BACKGROUND}}", `data:${mimeType};base64,${base64Image}`);
+
+	return respPage;
+}
